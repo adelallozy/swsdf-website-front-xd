83c99a8962d8e7a9f07dfe84325576304f4c06ca
swsdf.projects@gmail.com <adel.ellozy@gmail.com>
2022-12-01 15:10:13 +0300

    Change Content of Professional Diver License

--- src/views/ProfissionalDiving.vue
+++ src/views/ProfissionalDiving.vue
@@ -1,95 +1,83 @@
 <template>
   <section class="profissional-diving">
-    <h2 class="title">
+    <div class="content">
+      <h2 class="title" >
       <span><img src="@/assets/getintouch/2.png" alt="header" /></span>
       الغواص المحترف
     </h2>
+    </div>
+
 
     <div class="content">
       <div class="about">
         <div class="container">
           <div class="col-md-8 ms-auto">
             <div class="info p-30">
-              <p>
-                حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
-                القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في
-                الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها
-                تعطي توزيعاَ
-              </p>
+              <p styl="text-align: justify;">تعد وثيقة رسمية تصدر من الاتحاد السعودي للرياضات البحرية والغوص، توضح بأن الغواص الحاصل عليها مؤهل ومصرح له ومعتمد نظاماً من الاتحاد بممارسة أنشطة الغوص الاحترافية. ويعتبر هذا الترخيص شرط أساسي لكل غواص محترف يرغب في ممارسة أي من أنشطة الغوص الإحترافية (التدريب ، الإرشاد)، ولا يشترط الحصول على هذا الترخيص للأشخاص الذين يمارسون الغوص بشكل شخصي .</p>
               <a href="#" class="btn btn-primary custom-btn" id="register-btn">
-                تسجيل
+                التقديم
                 <span></span>
               </a>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="features">
-        <div>
-          <h3 class="title">مميزات خاصة</h3>
-
-          <div class="feature">
-            <ul class="list-unstyled">
-              <li>حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء</li>
-              <li>
-                لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي
-              </li>
-              <li>
-                للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.
-              </li>
-              <li>ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ</li>
-            </ul>
-          </div>
-          <div class="feature">
-            <h3 class="title">خطوات التسجيل</h3>
-            <ul class="list-unstyled">
-              <li>فتح رابط التسجيل</li>
-              <li>تعبئة النموج</li>
-              <li>مراجعة الطلب</li>
-              <li>اصدار الفاتورة</li>
-              <p>رسوم الرخصة 2000 ريال في عام 2022</p>
-            </ul>
-          </div>
-          <div class="feature">
-            <h3 class="title">خطوات التسجيل</h3>
-            <ul class="list-unstyled">
-              <li>فتح رابط التسجيل</li>
-              <li>تعبئة النموج</li>
-              <li>مراجعة الطلب</li>
-              <li>اصدار الفاتورة</li>
-              <p>رسوم الرخصة 2000 ريال في عام 2022</p>
-            </ul>
-          </div>
-          <div class="feature">
-            <h3 class="title">المستندات المطلوبة</h3>
-            <ul class="list-unstyled">
-              <li>شهادة عضوية سارية</li>
-              <li>بطاقة الهوية</li>
-              <li>شهادة عضوية سارية</li>
-              <li>بطاقة الهوية</li>
-              <li>شهادة عضوية سارية</li>
-              <li>بطاقة الهوية</li>
-            </ul>
-          </div>
-          <div class="feature">
-            <h3 class="title">الفحص الفني لمركز الغوص</h3>
+      <div class="container">
+        <div class="features">
+          <div class="list-block">
+            <h3 class="title">خطوات التقديم:</h3>
 
             <div class="feature">
-              <ul class="list-unstyled">
-                <li>حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء</li>
-                <li>
-                  لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي
-                </li>
-                <li>
-                  للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.
-                </li>
-                <li>
-                  ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ
-                </li>
-              </ul>
+              <ol  >
+                <li>إنشاء حساب للمستخدم .</li>
+                <li>عبئة نماذج ترخيص الغواص المحترف .</li>
+                <li>إرفاق جميع المستندات المطلوبة (نسخة الهوية ، صورة شخصية ، نسخة الرخصة الدولية ).</li>
+                <li>إرسال طلب الحصول على الترخيص.</li>
+                <li>يقوم مسئولي الاتحاد بمراجعة معلومات المتقدم والتحقق من صحة البيانات المدخلة . </li>
+                <li>تسديد رسوم الترخيص .</li>
+                <li>قبول الطلب وإصدار الترخيص. </li>
+              </ol>
             </div>
           </div>
+
+          <div class="list-block">
+            <h3 class="title">المستندات المطلوبة:</h3>
+
+            <div class="feature">
+              <ol  >
+                <li>نسخة الهوية الوطنية أو إقامة سارية . </li>
+                <li>صورة شخصية بخلفية بيضاء . </li>
+                <li>نسخة من رخصة الغواص المحترف الصادرة من أحد منظمات الغوص الدولية المعتمدة من الاتحاد في المملكة.</li>
+                <li>نسخة عقد العمل مع أحد مراكز الغوص المرخصة من وزارة الرياضة . </li>
+              </ol>
+            </div>
+          </div>
+ 
+          <div class="list-block">
+            <h3 class="title">الترخيص:</h3>
+
+            <div class="feature">
+              <ol  >
+                <li>يصدر الترخيص إلكترونياً بمجرد الموافقة على طلب المتقدم، وتكون متاحة على موقع الاتحاد ولا يشترط الحصول على نسخة مطبوعة من الترخيص  لممارسة أنشطة الغوص الإحترافية . </li>
+                <li>يوفر الاتحاد رابط للتحقق من صلاحية الترخيص. </li>
+                <li>يمكن للمتقدم طلب الحصول نسخة مطبوعة من الترخيص أثناء تقديم الطلب ، ويكون ذلك مقابل رسوم إضافية 50 ريال، ويتم إرسال الترخيص مطبوع بنسخة بلاستيكية إلى المتقدم على العنوان المحدد بالطلب. </li>
+                
+              </ol>
+            </div>
+          </div>
+
+           
+          <div class="list-block">
+            <h3 class="title">رسوم العضوية :</h3>
+
+            <div class="feature">
+              <ol  >
+                <li>رسوم إصدار وتجديد رخصة الغواص المحترف بمبلغ 300 ريال لمدة عام واحد من تاريخه وذلك للعام الميلادي 2022م . </li>
+                <li>رسوم طباعة بطاقة الترخيص 50 ريال عند الطلب . </li>
+              </ol>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -139,6 +127,14 @@ onMounted(() => {
       }
     }
   }
+  li {
+    max-width: 780px;
+    text-align: justify;
+  }
+  p {
+    max-width: 780px;
+    text-align: justify;
+  }
   .features {
     position: relative;
     // background: #f0f5fc 0% 0% no-repeat padding-box;
@@ -151,10 +147,16 @@ onMounted(() => {
       left: 0;
       width: 100%;
     }
+    .list-block {
+      margin-bottom: 1.5rem;
+      .title {
+        margin-bottom: 0.25rem ;
+      }
+    }
     h3 {
       font-size: 18px;
       @media (min-width: 768px) {
-        font-size: 24px;
+        font-size: 1.5rem;
       }
       text-align: start;
     }
@@ -168,7 +170,7 @@ onMounted(() => {
         color: #010d0f;
       }
       li {
-        margin-bottom: 20px;
+        // margin-bottom: 20px;
         font-weight: bold;
         position: relative;
         color: #010d0f;
